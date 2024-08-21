7775c030840255aeba166550deb2d71e4e625530
MKenesbayev <kenesbayev.murat96@gmail.com>
2024-08-21 17:05:06 +0500

    Add sql query builder

--- go.mod
+++ go.mod
@@ -2,4 +2,25 @@ module github.com/murmurat/querybuilder
 
 go 1.22.6
 
-require gopkg.in/guregu/null.v3 v3.5.0
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+	gitlab.com/golang-libs/databases.git v1.1.7
+	gopkg.in/guregu/null.v3 v3.5.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ sql/query_builder.go
@@ -0,0 +1,164 @@
+package repo
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
+	"gitlab.com/golang-libs/databases.git/relational"
+	"reflect"
+	"strings"
+)
+
+const (
+	update = "update"
+	insert = "insert"
+)
+
+func AddRow(tx *sqlx.Tx, object any, tableName string) error {
+	_, err := relational.DoX(func(tx *sqlx.Tx) (interface{}, error) {
+		query := generateInsertQuery(object, tableName)
+		valuesMap := getValuesMap(object)
+		return tx.NamedExec(query, valuesMap)
+	}, tx)
+	return err
+}
+
+func UpdateRow(tx *sqlx.Tx, object any, tableName string, keyField string) error {
+	_, err := relational.DoX(func(tx *sqlx.Tx) (interface{}, error) {
+		query := generateUpdateQuery(object, tableName, keyField)
+		valuesMap := getValuesMap(object)
+		return tx.NamedExec(query, valuesMap)
+	}, tx)
+	return err
+}
+
+func Exists(tx *sqlx.Tx, tableName string, fieldName string, val interface{}) (bool, error) {
+	_, err := relational.DoX(func(tx *sqlx.Tx) (interface{}, error) {
+		query := fmt.Sprintf(`SELECT 1 FROM %s WHERE %s = $1 LIMIT 1;`, tableName, fieldName)
+		var exists int
+		return nil, tx.QueryRowx(query, val).Scan(&exists)
+	}, tx)
+
+	if err != nil && errors.Is(err, sql.ErrNoRows) {
+		return false, nil
+	}
+	return true, err
+}
+
+func UpsertRow(tx *sqlx.Tx, object any, tableName string, keyField string) (opType string, err error) {
+	_, err = relational.DoX(func(tx *sqlx.Tx) (interface{}, error) {
+		keyValue, err := getFieldValueByTag(object, keyField)
+		if err != nil {
+			return nil, err
+		}
+
+		exists, err := Exists(tx, tableName, keyField, keyValue)
+		if err != nil {
+			return nil, err
+		}
+
+		if exists {
+			opType = update
+			err = UpdateRow(tx, object, tableName, keyField)
+			return nil, err
+		}
+
+		opType = insert
+		err = AddRow(tx, object, tableName)
+		return nil, err
+	}, tx)
+
+	return opType, err
+}
+
+func generateInsertQuery(obj interface{}, tableName string) string {
+	t := reflect.TypeOf(obj)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	var columns []string
+	var placeholders []string
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		dbTag := field.Tag.Get("db")
+		if dbTag != "" {
+			columns = append(columns, dbTag)
+			placeholders = append(placeholders, ":"+dbTag)
+		}
+	}
+
+	columnsStr := strings.Join(columns, ", ")
+	placeholdersStr := strings.Join(placeholders, ", ")
+
+	query := fmt.Sprintf(`INSERT INTO %s (%s) VALUES (%s);`, tableName, columnsStr, placeholdersStr)
+	return query
+}
+
+func generateUpdateQuery(obj interface{}, tableName string, keyField string) string {
+	t := reflect.TypeOf(obj)
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	var updateFields []string
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		dbTag := field.Tag.Get("db")
+		if dbTag != "" && dbTag != keyField {
+			updateFields = append(updateFields, fmt.Sprintf("%s = :%s", dbTag, dbTag))
+		}
+	}
+
+	updateFieldsStr := strings.Join(updateFields, ", ")
+	query := fmt.Sprintf(`UPDATE %s SET %s WHERE %s = :%s;`, tableName, updateFieldsStr, keyField, keyField)
+	return query
+}
+
+func getFieldValueByTag(obj interface{}, tagName string) (interface{}, error) {
+	v := reflect.ValueOf(obj)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("expected a struct or a pointer to a struct, got %T", obj)
+	}
+
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if dbTag := field.Tag.Get("db"); dbTag == tagName {
+			return v.Field(i).Interface(), nil
+		}
+	}
+
+	return nil, fmt.Errorf("no field with db tag %q found", tagName)
+}
+
+func getValuesMap(obj interface{}) map[string]interface{} {
+	m := make(map[string]interface{})
+	t := reflect.TypeOf(obj)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tagName := field.Tag.Get("db")
+		if tagName == "" {
+			continue
+		}
+		val, _ := getFieldValueByTag(obj, tagName)
+
+		// cast slices to db array
+		if field.Type.Kind() == reflect.Slice {
+			val = pq.Array(val)
+		}
+
+		m[tagName] = val
+	}
+
+	return m
+}
